c5d3826e8b5c31f5f8844fc467953cf337c443aa
Nickteca <isaaclunaavila@gmail.com>
2023-11-11 16:26:34 -0600

    modified:   app/Controllers/Admin/CEquipo.php 	modified:   app/Views/Admin/VEquipo.php

--- app/Controllers/Admin/CEquipo.php
+++ app/Controllers/Admin/CEquipo.php
@@ -3,9 +3,15 @@
 namespace App\Controllers\Admin;
 
 use App\Controllers\BaseController;
+use App\Models\MEquipo;
+use App\Models\MLiga;
 
 class CEquipo extends BaseController {
     public function index() {
-        return view('Admin/VEquipo');
+        $equipos = new MEquipo();
+        $liga = new MLiga();
+        $nligas['nligas'] = $liga->findAll();
+        $nequipos['nequipos'] = $equipos->findAll();
+        return view('Admin/VEquipo', $nequipos, $nligas);
     }
 }
